ea13d837e1a471fb9da6af996fd2de83f7ed6e06
Kurtuluş Mert Küçük <48620003+kmkucuk@users.noreply.github.com>
2024-04-02 18:48:14 +0300

    Uploaded all files

---
+++ check_overlapping_ids/tracker.js
@@ -0,0 +1,127 @@
+
+const fs = require('fs');
+
+shelf_names = ['list','experimentRegistry','performanceRegistry','fitClashRegistry','attemptRegistry']
+
+var shelves = {}
+var added_shelves = {}
+
+for (currentShelf of shelf_names){
+    // console.log(currentShelf)
+    shelfPath = '.\\existing_shelves\\' + currentShelf +'.json'
+    shelves[currentShelf] = require(shelfPath)
+    // console.log(shelves[currentShelf])
+
+    shelfPath = '.\\added_shelves\\added_' + currentShelf +'.json'
+    added_shelves[currentShelf] = require(shelfPath)
+}
+
+
+
+
+// loop over keys to find if one of added IDs exist in existing shelves
+var match_object = {}
+
+// noOverlap variable used for identifying if there is "any" overlap in 
+// ID names between added and existing shelves. 
+
+// If there are no overlaps, code continues and creates the combined shelves
+noOverlap = 1
+
+
+var overlappindParticipants = []
+for (currentShelf of shelf_names){
+    
+    existing_ids = Object.keys(shelves[currentShelf])
+    added_ids    = Object.keys(added_shelves[currentShelf])
+
+
+    for (currentID of added_ids){
+
+
+        if (!Object.keys(match_object).includes(currentID)){
+
+            match_object[currentID] = {}
+
+        }
+            
+
+        if (existing_ids.includes(currentID)){
+
+            match_object[currentID][currentShelf] = "overlap"
+            overlappindParticipants.push(currentID)
+            noOverlap = 0
+        } else {
+
+            match_object[currentID][currentShelf] = "good"
+
+        }
+            
+
+    }
+
+}
+
+
+
+// initialize months as a vector for date save
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// initialize date variable for retrieving month info
+const d = new Date();
+// get day of the month (1-31)
+currentDay = d.getDate()
+// get month of the year as text ("april" etc.)
+currentMonth = months[d.getMonth()];
+// get current year
+currentYear = d.getFullYear()
+
+
+const folderName = currentDay.toString()+currentMonth+currentYear.toString()
+
+
+var combined_shelves = {} 
+
+
+Object.assign(combined_shelves,shelves)
+
+// console.log(combined_shelves)
+
+// if there was no overlap, merge existing and added shelves here
+if (noOverlap){
+    // combine existing and added shelves using deep merge function    
+
+    for (currentShelf of shelf_names){
+
+        Object.assign(combined_shelves[currentShelf],added_shelves[currentShelf])
+        
+        console.log(combined_shelves)
+        console.log(combined_shelves[currentShelf])
+        
+        shelfPath = '.\\combined_shelves\\'+folderName+'\\'
+
+        // generate the folder for this date if it does not exists
+        if (!fs.existsSync(shelfPath)){
+            fs.mkdirSync(shelfPath);  }
+
+        // turn shelf object into string for writing
+        writtenShelf = JSON.stringify(combined_shelves[currentShelf])
+        // create the full file path for writing
+        filePath = shelfPath + 'combined_' + currentShelf +'.json'
+        // Write the JSON string 
+        fs.writeFile(filePath, writtenShelf, (err) => {
+            if (err) {
+                throw err;
+            }
+            
+        });
+        console.log(currentShelf + ' shelf is saved as ' + 'combined_'+ currentShelf +'.json in '+folderName+' folder\n');
+    }
+
+
+} else {
+    // throw an error due to overlap in IDs
+    console.error('Shelf merge failed due to overlap, check .csv file for the following overlapping IDs:');
+    // print out overlapping IDs just once 
+    console.error(JSON.stringify(...new Set(overlappindParticipants)))
+
+}
---
+++ generate_ids/id_generation_revised.js
@@ -0,0 +1,144 @@
+
+
+const XLSX = require('xlsx');
+const fs = require('fs');
+const Docxtemplater = require('docxtemplater');
+const path = require('path');
+
+const scriptDirectory = __dirname; // Get the directory of the current script
+const sheetFile       = path.join(scriptDirectory,'cb_experiment_id.xlsx')
+// Load the Excel file
+const workbook = XLSX.readFile(sheetFile); // Replace 'cb_id_sheet.xlsx' with the path to your Excel file
+
+// Assume the data is in the first sheet of the workbook
+const sheetName = workbook.SheetNames[0];
+const worksheet = workbook.Sheets[sheetName];
+
+// Define the list variable to store the values from the first column
+const ids = {};
+
+ids.sub_id      = []
+ids.teacher_id  = []
+ids.district_id = []
+ids.cbpasssage  = []
+ids.cbspacing   = []
+ids.cbtime      = []
+// Loop through the worksheet to extract values from the first column
+for (let rowIndex = 2; rowIndex < 77; rowIndex++) {
+  const id_cell             = worksheet[`B${rowIndex}`]; // Assuming the values are in column A (adjust as needed)
+  const districtid_cell     = worksheet[`C${rowIndex}`];
+  const teacherid_cell      = worksheet[`D${rowIndex}`];  
+  const cbpas_cell          = worksheet[`F${rowIndex}`];
+  const cbspa_cell          = worksheet[`G${rowIndex}`];
+  const cbtim_cell          = worksheet[`H${rowIndex}`];
+  if (!id_cell || !id_cell.v) {
+    // Break when there are no more values in the first column
+    continue;
+  }
+
+  ids["sub_id"].push(id_cell.v);
+  ids["district_id"].push(districtid_cell.v);
+  ids["teacher_id"].push(teacherid_cell.v);  
+  ids["cbpasssage"].push(cbpas_cell.v);
+  ids["cbspacing"].push(cbspa_cell.v);
+  ids["cbtime"].push(cbtim_cell.v);
+
+}
+
+
+// console.log(ids["sub_id"]);
+// console.log(ids["teacher_id"]);
+// console.log(ids["cbpasssage"]);
+// Output the list of IDs
+// for (const curid of ids){
+//     console.log(curid);
+// }
+
+
+var shelves = {}
+ch_list = {}
+ch_attemptRegistry = {}
+ch_fitClashRegistry = {} 
+ch_experimentRegistry = {}
+ch_performanceRegistry = {}
+
+for (var i = 0; i < ids["sub_id"].length; i++){
+  ch_list[ids["sub_id"][i]]                 = {"teacherID": ids["teacher_id"][i],"districtID": ids["district_id"][i],"counterbalance_group": [ids["cbpasssage"][i],ids["cbspacing"][i],ids["cbtime"][i]]}
+  ch_attemptRegistry[ids["sub_id"][i]]      = [0,0,0]
+  ch_fitClashRegistry[ids["sub_id"][i]]     = [{"fit": [], "clash": [] }, {"fit": [], "clash": [] } ]
+  ch_experimentRegistry[ids["sub_id"][i]]   = {"name": "none","t1_status": "notStarted","t1_leftTrial": 0, "t1_trialOrder" : [[],[],[]],"t2_status": "notStarted","t2_leftTrial": 0, "t2_trialOrder" : [[],[],[]],"t3_status": "notStarted","t3_leftTrial": 0,"t3_trialOrder" : [[],[],[]],"completionDates": ["undefined","undefined","undefined"]}
+  ch_performanceRegistry[ids["sub_id"][i]]  = [
+      {
+        "sp1": { "setid": [], "wpm": [], "accuracy": [] },
+        "sp2": { "setid": [], "wpm": [], "accuracy": [] },
+        "sp3": { "setid": [], "wpm": [], "accuracy": [] }
+      },
+      {
+        "sp1": { "setid": [], "wpm": [], "accuracy": [] },
+        "sp2": { "setid": [], "wpm": [], "accuracy": [] },
+        "sp3": { "setid": [], "wpm": [], "accuracy": [] }
+      }
+    ]
+
+}
+
+
+// console.log("ID LIST")
+// console.log(JSON.stringify(ch_list))
+// console.log("\nATTEMPT REGISTRY")
+// console.log(JSON.stringify(ch_attemptRegistry))
+// console.log("\nFIT CLASH REGISTRY")
+// console.log(JSON.stringify(ch_fitClashRegistry))
+// console.log("\nEXPERIMENT REGISTRY")
+// console.log(JSON.stringify(ch_experimentRegistry))
+// console.log("\nPERFORMANCE REGISTRY")
+// console.log(JSON.stringify(ch_performanceRegistry))
+
+
+shelves["list"] = ch_list
+shelves["experimentRegistry"] = ch_experimentRegistry
+shelves["performanceRegistry"] = ch_performanceRegistry
+shelves["fitClashRegistry"] = ch_fitClashRegistry
+shelves["attemptRegistry"] = ch_attemptRegistry
+
+console.log('++++++++++++++++++++++\n\n')
+var shelf_names = Object.keys(shelves)
+
+
+// initialize months as a vector for date save
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// initialize date variable for retrieving month info
+const d = new Date();
+// get day of the month (1-31)
+currentDay = d.getDate()
+// get month of the year as text ("april" etc.)
+currentMonth = months[d.getMonth()];
+// get current year
+currentYear = d.getFullYear()
+
+
+const folderName = currentDay.toString()+currentMonth+currentYear.toString()
+
+
+
+for (currentShelf of shelf_names){
+        
+  shelfPath = '.\\generated_shelves\\'+folderName+'\\'
+  // generate the folder for this date if it does not exists
+  if (!fs.existsSync(shelfPath)){
+    fs.mkdirSync(shelfPath);  }
+  filePath = shelfPath + 'added_' + currentShelf +'.json'
+
+  currentWrittenShelf = JSON.stringify(shelves[currentShelf])
+  // Write the JSON string to a file named 'user.json'
+  fs.writeFile(filePath, currentWrittenShelf, (err) => {
+      if (err) {
+          throw err;
+      }
+      
+  });
+  
+  console.log(currentShelf + ' shelf is saved as ' + 'added_'+ currentShelf +'.json in '+folderName+' folder\n');
+
+}
+
